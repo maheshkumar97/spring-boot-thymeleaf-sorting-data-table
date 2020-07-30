c95dc1badc1fbc0b11cce44789ea49ed73f35767
Maheshkumar <Maheshkumar@DESKTOP-HMFG54U>
2020-07-30 15:24:44 +0530

    Updated issues to make application more configurable.
    Make sure you should have installed with h2 database installed and it
    would be opened with Generic H2 (Server) only.\

--- SpringBootThymeleafDataDemo/src/main/java/com/example/thymeleaf/thymeleafdemo/controller/EmployeeRestController.java
+++ SpringBootThymeleafDataDemo/src/main/java/com/example/thymeleaf/thymeleafdemo/controller/EmployeeRestController.java
@@ -49,11 +49,6 @@ public class EmployeeRestController {
 	@PostMapping("/save")
 	public String saveEmployee(@ModelAttribute("employee") Employee theEmployee) {
 
-		// also just in case they pass an id in JSON ... set id to 0
-		// this is to force a save of new item ... instead of update
-
-		// theEmployee.setId(0);
-
 		employeeService.save(theEmployee);
 
 		return "redirect:/api/employees";
@@ -78,62 +73,6 @@ public class EmployeeRestController {
 		employeeService.deleteById(id);
 
 		return "redirect:/api/employees";
-	}
-	// add mapping for GET /employees/{employeeId}
-
-	@GetMapping("/employees/{employeeId}")
-	public Employee getEmployee(@PathVariable int employeeId) {
-
-		Employee theEmployee = employeeService.findById(employeeId);
-
-		if (theEmployee == null) {
-			throw new RuntimeException("Employee id not found - " + employeeId);
-		}
-
-		return theEmployee;
-	}
-
-	// add mapping for POST /employees - add new employee
-
-	@PostMapping("/employees")
-	public Employee addEmployee(@RequestBody Employee theEmployee) {
-
-		// also just in case they pass an id in 9JSON ... set id to 0
-		// this is to force a save of new item ... instead of update
-
-		theEmployee.setId(0);
-
-		employeeService.save(theEmployee);
-
-		return theEmployee;
-	}
-
-	// add mapping for PUT /employees - update existing employee
-
-	@PutMapping("/employees")
-	public Employee updateEmployee(@RequestBody Employee theEmployee) {
-
-		employeeService.save(theEmployee);
-
-		return theEmployee;
-	}
-
-	// add mapping for DELETE /employees/{employeeId} - delete employee
-
-	@DeleteMapping("/employees/{employeeId}")
-	public String deleteEmployee(@PathVariable int employeeId) {
-
-		Employee tempEmployee = employeeService.findById(employeeId);
-
-		// throw exception if null
-
-		if (tempEmployee == null) {
-			throw new RuntimeException("Employee id not found - " + employeeId);
-		}
-
-		employeeService.deleteById(employeeId);
-
-		return "Deleted employee id - " + employeeId;
 	}
 
 }
--- SpringBootThymeleafDataDemo/src/main/java/com/example/thymeleaf/thymeleafdemo/dao/DataSourceConfig.java
+++ SpringBootThymeleafDataDemo/src/main/java/com/example/thymeleaf/thymeleafdemo/dao/DataSourceConfig.java
@@ -13,7 +13,7 @@ public class DataSourceConfig {
     public DataSource getDataSource() {
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
         dataSourceBuilder.driverClassName("org.h2.Driver");
-        dataSourceBuilder.url("jdbc:h2:mem:test");
+        dataSourceBuilder.url("jdbc:h2:tcp://localhost/~/test");
         dataSourceBuilder.username("SA");
         dataSourceBuilder.password("");
         return dataSourceBuilder.build();
